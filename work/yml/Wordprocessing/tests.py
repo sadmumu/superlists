fc3f015b8f445d4e4248eef3c92826ab4650fc58
Y2017211927 <0884d2140680f4881fd5c01a6843e1e0@huawei.com>
2020-05-15 10:17:17 +0800

    更新了部分单元测试

--- work/yml/Wordprocessing/tests.py
+++ work/yml/Wordprocessing/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from .models import Commdisaster
+from Wordprocessing.views import home_page
+from django.urls import resolve
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 # Create your tests here.
 class TestModel(TestCase):
@@ -7,3 +11,16 @@ class TestModel(TestCase):
         temp = Commdisaster.objects.all()
         self.assertEqual(temp.count(),3)
         # self.assertEqual(temp[0].name,'yml')
+        
+    def test_resolve_to_home_page_view(self):
+        found=resolve('/')
+        self.assertEqual(found.func,home_page)
+    
+    def test_home_page_return_correct_html(self):
+        reponse=self.client.get('/')
+        
+        html=response.content.decode('utf8')
+        self.assertTrue(html.startwith('<html>'))
+        self.assertTrue(html.strip().endwith('</html>'))
+        
+        self.assertTemplateUsed(reponse,'index.html')
